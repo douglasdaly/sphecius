73058c6a080709b7a9df508ad05a78f79bd56dd9
Doug <douglas.daly89@gmail.com>
2017-01-07 13:58:57 -0500

    Additional dev work on Morse code class

--- src/sphecius/translators/morse.py
+++ src/sphecius/translators/morse.py
@@ -16,10 +16,13 @@ class Morse(object):
 
     def __init__(self):
         """Default Constructor"""
-        self.__dict_morse = self.__generate_morse_code_dictionary()
+
+        dict_tup = self.__generate_morse_code_dictionaries()
+        self.__dict_to_morse = dict_tup[0]
+        self.__dict_from_morse = dict_tup[1]
     
     
-    def __generate_morse_code_dictionary(self):
+    def __generate_morse_code_dictionaries(self):
         """Generates a dict() object of Morse Code"""
         
         dict_morse = dict()
@@ -61,20 +64,58 @@ class Morse(object):
         dict_morse['---..'] = '8'
         dict_morse['----.'] = '9'
         
-        return dict_morse
+        dict_letters = dict()
+        for key in dict_morse.keys():
+            dict_letters[dict_morse[key]] = key
+        
+        return (dict_letters, dict_morse)
     
     
-    def convert_from_morse(self, ToConvert, MinLetters=1):
-        """Converts a string of . and -'s to Letters"""
+    def convert_to(self, to_convert):
+        """Converts the given letter string to morse code"""
+        
+        to_convert = to_convert.lower()
+        output = '|'
+        for l in range(len(to_convert)):
+            letter = to_convert[l]
+            if letter == ' ':
+                output += ' '
+            elif letter in self.__dict_to_morse.keys():
+                output += self.__dict_to_morse[letter]
+            else:
+                output += '?'
+            output += '|'
+        
+        return output.strip()
+                
+    
+    def convert_from(self, to_convert):
+        """Converts a morse string with spaces to the english letters"""
+        
+        # - Divy it up
+        if to_convert.find('|') > -1:
+            arr = to_convert.strip('|').split('|')
+        else:
+            arr = to_convert.strip().split(' ')
+        
+        output = ''
+        for word in arr:
+            output += self.convert_from_no_spaces(word, len(word))
+        
+        return output
+    
+    
+    def convert_from_no_spaces(self, to_convert, min_letters=1):
+        """Converts a string of . and -'s to Letters with no spaces/breaks in it"""
         
         cStr = ''
         outStr = ''
         
-        for i in range(len(ToConvert)):
-            cStr += ToConvert[i]
-            if len(cStr) == MinLetters:
-                if cStr in self.__dict_morse.keys():
-                    outStr += self.__dict_morse[cStr]
+        for i in range(len(to_convert)):
+            cStr += to_convert[i]
+            if len(cStr) == min_letters:
+                if cStr in self.__dict_from_morse.keys():
+                    outStr += self.__dict_from_morse[cStr]
                 else:
                     outStr += ' '
                 cStr = ''
