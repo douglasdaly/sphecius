5cdfe83e0491f157f84a4a635d62fb26d8a659a3
Doug <douglas.daly89@gmail.com>
2017-01-08 00:41:03 -0500

    Empty Vigenere and Caesar cipher classes

---
+++ src/sphecius/ciphers/caesar.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  7 22:19:59 2017
+
+@author: doug
+"""
+
+#
+#   Imports
+#
+
+
+#
+#   Class
+#
+
+class Caesar(object):
+    """Caesar Cipher Class"""
+    
+    def __init__(self):
+        """Default Constructor"""
+        
+        
---
+++ src/sphecius/ciphers/vigenere.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  7 22:17:38 2017
+
+@author: doug
+"""
+
+#
+#   Imports
+#
+
+
+#
+#   Class
+#
+
+class Vigenere(object):
+    """Vigenere Cipher Class"""
+    
+    def __init__(self):
+        """Default Constructor"""
+        
+        
