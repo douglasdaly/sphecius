494ab20e440578dd9ca6a95f4276ab9c56f775a8
Doug <douglas.daly89@gmail.com>
2017-01-21 12:22:54 -0500

    Continued work on NGram class

--- src/sphecius/cryptanalysis/ngram.py
+++ src/sphecius/cryptanalysis/ngram.py
@@ -8,7 +8,12 @@ Created on Sat Jan  21 11:21:00 2017
 #
 #   Imports
 #
+import os.path
 
+#
+#   Variables
+#
+ngram_base_dir = "../resources/ngrams/"
 
 #
 #   Class
@@ -19,4 +24,36 @@ class NGram(object):
 
     def __init__(self, n, language='english'):
         """Default Constructor"""
+        filename = language + "_" + str(n) + "grams.txt"
+
+        # - Throw Exception if no resource file for this
+        if not os.path.isfile(ngram_base_dir + filename):
+            raise Exception("No N-Gram Resource File for N=" + str(n) + " and lang=" + language + " exists.")
+
+        self.__n = n
+        self.__gram_dict = self.__load_grams_from_file(ngram_base_dir + filename)
+
+    @staticmethod
+    def __load_grams_from_file(filepath):
+        """Loads the N-Gram Dict from File"""
+        occurrences = dict()
+        run_tot = 0.0
+
+        # - Load from File
+        with open(filepath, 'r') as f:
+            for line in f:
+                splat = line.split(' ')
+                tnum = float(splat[1])
+                occurrences[splat[0].upper()] = tnum
+                run_tot += tnum
+
+        # - Convert to Probabilities/Rates
+        output = dict()
+        for (k, v) in occurrences.iteritems():
+            output[k] = v / run_tot
+
+        return output
+
+    def get_occurrences(self, text, remove_spaces=False):
+        """Gets occurrences of this N-Gram from the given Text"""
         
