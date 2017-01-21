ed8741b93f1f0e5b5d3b4343af3938b4a1dce84d
Doug <douglas.daly89@gmail.com>
2017-01-21 11:37:16 -0500

    Moved/Renamed some resource files and added ngram class

---
+++ src/sphecius/cryptanalysis/ngram.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  21 11:21:00 2017
+
+@author: doug
+"""
+
+#
+#   Imports
+#
+
+
+#
+#   Class
+#
+
+class NGram(object):
+    """N-Gram class for Text Analysis"""
+
+    def __init__(self, n, language='english'):
+        """Default Constructor"""
+        
