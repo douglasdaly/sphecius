b5b5973c341e57a7b3c45a0e762f2a6ba34d9433
Doug <douglas.daly89@gmail.com>
2017-04-07 07:38:29 -0400

    Added base translator class

--- sphecius/translators/base.py
+++ sphecius/translators/base.py
@@ -10,6 +10,7 @@ Base.py
 #
 #   Imports
 #
+from ..alphabets import Alphabet
 
 
 #
@@ -21,5 +22,25 @@ class Translator(object):
     Base Translator Object
     """
 
-    def __init__(self):
-        """Default Constructor"""
+    def __init__(self, alphabet_from, alphabet_to):
+        """Default Constructor
+
+        :param Alphabet alphabet_from: The Alphabet we're translating from
+        :param Alphabet alphabet_to: The Alphabet we're translating to
+
+        """
+        self._alphabet_from = alphabet_from
+        self._alphabet_to = alphabet_to
+
+    def translate(self, text):
+        """Translates the given Text
+
+        Translates the given text, in the 'From' Alphabet to the 'To' Alphabet.
+
+        :param str text: Text to translate
+
+        :return: Translated text
+        :rtype: str
+
+        """
+        raise NotImplementedError()
