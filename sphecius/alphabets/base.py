861f74406d3adfcba1b86a0d8e86fe05a4bb57d0
Doug <douglas.daly89@gmail.com>
2017-09-30 16:49:57 -0400

    Indexing/getitem added to alphabet base class

--- sphecius/alphabets/base.py
+++ sphecius/alphabets/base.py
@@ -28,15 +28,30 @@ class Alphabet(object, metaclass=ABCMeta):
         """
         pass
 
-    def get_alphabet(self):
-        """ Abstract get Alphabet Method
+    @property
+    def alphabet(self):
+        """ Alphabet Property
         """
         return self._alphabet
 
     def get_alphabet_list(self):
         """ Gets the Alphabet in List format
         """
-        return list(self.get_alphabet())
+        return list(self._alphabet)
+
+    def __getitem__(self, item):
+        """ Gets the Letter at the given index or the index at the given letter
+
+        :param item: Either the Letter to get the index of or the index of the letter to get
+
+        :return: Either the Letter at the given Index or Index of the given Letter
+        :rtype: str or int
+
+        """
+        if type(item) == str:
+            return self.a2i(item)
+        else:
+            return self.i2a(item)
 
     def i2a(self, index):
         """ Gets the Letter at the given Index
@@ -52,9 +67,21 @@ class Alphabet(object, metaclass=ABCMeta):
         return self._alphabet[index-1]
 
     def a2i(self, letter):
-        """Base get Index of Letter Method"""
+        """ Base get Index of Letter Method
+
+        :param str letter: Letter to get the index of
+
+        :return: Index of the letter given (1-based indexing)
+        :rtype: int
+
+        """
         return self._alphabet.find(letter.upper()) + 1
 
     def size(self):
-        """Length of the alphabet"""
+        """ Length of the alphabet
+
+        :return: Length of this alphabet
+        :rtype: int
+
+        """
         return len(self._alphabet)
